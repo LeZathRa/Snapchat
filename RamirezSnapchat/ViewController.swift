307874cb7479519228501f55e9a269256c3f2478
Less <lessiel.rh@gmail.com>
2024-05-22 12:57:09 -0500

    Auth Firebase

--- RamirezSnapchat/ViewController.swift
+++ RamirezSnapchat/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
-class ViewController: UIViewController {
+class iniciarSesionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {(user, error) in print("Intentando Iniciar Sesion")
+            if error != nil{
+                print("Se presento el siguiente error:\(error)")
+            } else {
+                print("Inicio de sesion exitoso")
+            }
+        }
+    }
 }
 
